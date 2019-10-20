c7071bc55e9cf914e77ac4c0b6f4f0e18839beb0
Dmitriev Mihail <derpiranha@gmail.com>
2019-10-20 21:16:39 +0300

    user struct added (#9)

--- src/murcure/client_storage.cr
+++ src/murcure/client_storage.cr
@@ -1,18 +1,21 @@
 module Murcure
   class ClientStorage
     def initialize
-      @clients = {} of UInt32 => NamedTuple(session_id: UInt32, handler: Murcure::ClientHandler, machine: Murcure::ClientState, attrs: Hash(Symbol, (String | Int32 | UInt32| Symbol | Nil | Array(String))))
+      @clients = {} of UInt32 => Murcure::ClientStruct 
+      # NamedTuple(session_id: UInt32, handler: Murcure::ClientHandler, machine: Murcure::ClientState, attrs: Hash(Symbol, (String | Int32 | UInt32| Symbol | Nil | Array(String))))
     end
 
+    # def initialize(@session_id, @handler, @machine, @attrs); end
     def add_client(session_id : UInt32, handler : Murcure::ClientHandler, machine : Murcure::ClientState) : Bool
-      @clients[session_id] = { session_id: session_id, handler: handler, machine: machine, attrs: {} of Symbol => (String | Int32 | UInt32 | Symbol | Nil | Array(String))}
+      @clients[session_id] = Murcure::ClientStruct.new(session_id, handler, machine, {} of Symbol => Murcure::ClientStruct::Attributes) 
+      # { session_id: session_id, handler: handler, machine: machine, attrs: {} of Symbol => (String | Int32 | UInt32 | Symbol | Nil | Array(String))}
       true
     end
 
     def channel(session_id : (UInt32 | Nil))
       raise "session_id cannot be bil" if session_id.nil?
 
-      @clients[session_id.not_nil!][:handler].client_channel
+      @clients[session_id.not_nil!].handler.client_channel
     end
 
     def clients
@@ -26,13 +29,13 @@ module Murcure
       client || raise "can not find client with session_id='#{session_id.not_nil!}'"
     end
 
-    def update_attr(session_id : (UInt32 | Nil), attr_name : Symbol, attr_value : (String | Int32 | UInt32 | Symbol | Nil | Array(String))) : Bool
+    def update_attr(session_id : (UInt32 | Nil), attr_name : Symbol, attr_value : Murcure::ClientStruct::Attributes) : Bool
       return false if session_id.nil?
 
       client = @clients[session_id.not_nil!]
       return false unless client
 
-      client[:attrs][attr_name] = attr_value
+      client.attrs[attr_name] = attr_value
       true
     end
   end
